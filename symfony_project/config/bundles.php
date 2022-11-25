2252e7731eb1ca6ad038530ef487ccc6d99a8291
Leocoyere <leo.coyere94@gmail.com>
2022-11-25 15:30:55 +0100

    Branch leolelavabo

--- symfony_project/config/bundles.php
+++ symfony_project/config/bundles.php
@@ -15,4 +15,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
--- symfony_project/src/Controller/UserController.php
+++ symfony_project/src/Controller/UserController.php
@@ -4,12 +4,21 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     #[Route('/user-list', name: 'user_list')]
     public function userList(UserRepository $userRepository)
+    {
+        return $this->json([
+            'users' => $userRepository->findAll()
+        ], 200, [], ['groups' => 'main']);
+    }
+
+    #[Route('/login', name: 'user_login')]
+    public function login(string $appSecret): JsonResponse
     {
         return $this->json([
             'users' => $userRepository->findAll()
